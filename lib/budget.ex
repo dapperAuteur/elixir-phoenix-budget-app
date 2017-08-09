48058905adcfd6f083f7ec17adc237fef7bcb11b
Anthony McDonald <a@awews.com>
2017-08-09 18:25:00 -0500

    replace hard coded db with eco repository

--- lib/budget.ex
+++ lib/budget.ex
@@ -9,7 +9,7 @@ defmodule Budget do
     # Define workers and child supervisors to be supervised
     children = [
       # Start the Ecto repository
-      # supervisor(Budget.Repo, []),
+      supervisor(Budget.Repo, []),
       # Start the endpoint when the application starts
       supervisor(Budget.Endpoint, []),
       # Start your own worker by calling: Budget.Worker.start_link(arg1, arg2, arg3)
--- lib/budget/repo.ex
+++ lib/budget/repo.ex
@@ -1,22 +1,3 @@
 defmodule Budget.Repo do
-  # use Ecto.Repo, otp_app: :budget
-  @moduledoc """
-  In memory repository
-  """
-  def all(Budget.User) do
-    [%Budget.User{id: "1", name: "Jose", username: "josevalim", password: "elixir"},
-    %Budget.User{id: "2", name: "Bruce", username: "redrapids", password: "7langs"},
-    %Budget.User{id: "3", name: "Chris", username: "chrismccord", password: "phx"}]
-  end
-  def all(_module), do: []
-
-  def get(module, id) do
-    Enum.find all(module), fn map -> map.id == id end
-  end
-
-  def get_by(module, params) do
-    Enum.find all(module), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end
-  end
+  use Ecto.Repo, otp_app: :budget
 end
